d304aafe323deae2ad90f149826b70f4b6cb2979
Bhoomika-Agrawal <bhoomikaagrawal955@gmail.com>
2025-11-30 16:20:32 +0530

    updated integration

--- hybrid_roadmap/api.py
+++ hybrid_roadmap/api.py
@@ -191,5 +191,5 @@ def full_process_route():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5003))
+    port = int(os.environ.get('PORT', 5002))
     app.run(debug=True, host='0.0.0.0', port=port)
